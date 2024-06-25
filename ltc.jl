27a3113a02d768a25270c21690da2dd14827cf38
dreivmeister <felix.brandt@fh-bielefeld.de>
2024-06-25 17:08:51 +0200

    somethgin like this for package

--- ltc.jl
+++ ltc.jl
@@ -179,16 +179,3 @@ function (a::LTC)(input, hx=nothing; return_seq=false)
     end
     return h_out', h_state'
 end
-
-
-input_dimension = 2
-num_units = 5 # output_dimension = num_units
-time_length = 1
-batch_size = 3
-
-ltc = LTC(input_dimension, num_units)
-
-input = rand(time_length, input_dimension, batch_size)
-out, state = ltc(input; return_seq=true)
-println(size(out), " ", size(state)) # expect: if return_seq==false: (num_units, batch_size) and (num_units, batch_size)
-                                     #         else (time_length, num_units, batch_size) and (num_units, batch_size)
---
+++ main.jl
@@ -0,0 +1,5 @@
+module ltc
+
+include("ltc.jl")
+
+end
